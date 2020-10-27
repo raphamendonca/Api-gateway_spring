41d9d777f4553c2ad841ca6bcfe2081117964aa0
Raphael Alonso de Mendonça <Raphael.mendonca@hotmail.com>
2020-10-27 17:19:47 -0300

    adição do codigo

---
+++ gateway/src/main/kotlin/rapha/gateway/GatewayApplication.kt
@@ -0,0 +1,18 @@
+package rapha.gateway
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec
+import org.springframework.cloud.gateway.route.builder.PredicateSpec
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayApplication>(*args)
+}
